1ac7b9d47be4456a26420f6439d878bd33e0406e
Johan Kämpe <kampe.johan@gmail.com>
2017-01-17 17:52:26 +0100

    Template for exercise source files.

---
+++ Template/e0x_0x.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Exercise from book
+ *  C++ Direkt by Jan Skansholm
+ *  http://skansholm.com
+ *
+ *  Sida X
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	return 0;
+}
