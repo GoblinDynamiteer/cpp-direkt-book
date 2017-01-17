424df18ef3281d7ccfb0ad181e55b7cefc6cfd84
Johan Kämpe <kampe.johan@gmail.com>
2017-01-17 17:45:00 +0100

    Exercise 02.02

---
+++ ch02/exercise02_02/e02_02.cpp
@@ -0,0 +1,43 @@
+/*
+ * e02_01.cpp
+ *
+ *  Created on: 17 jan. 2017
+ *      Author: kampe_000
+ *
+ *  Sida 63
+ *  Komplettera programmet i övning 2.1:
+ *  +In: bensinförbrukning, som ett reellt tal
+ *  +Ut: Bensinförbrukning och liter per mil
+ *  Värdena ska vara högerjusterade, ha max två decimaltecken
+ */
+
+#include <iostream>
+
+/*	 Contains output manipulation functions	*/
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	int meter_idag, meter_ar;
+	double bensin;
+	cout << "Ange dagens mätarställning: ";
+	cin >> meter_idag;
+	cout << "Ange mätarställning för ett år sedan: ";
+	cin >> meter_ar;
+	cout << "Ange total förbrukad bensin: ";
+	cin >> bensin;
+
+	/*	 setw() sets width of output, fills left space with blanks	*/
+
+	cout << "Antal körda mil: "
+			<< setw(10) << meter_idag - meter_ar << endl;
+	cout << "Antal liter bensin: "
+
+	/*	'fixed' sets floating point values to fixed length,
+	 * 	specified with setprecision()	*/
+			<< setw(10) << fixed << setprecision(2)<< bensin << endl;
+	cout << "Förbrukning per mil: "
+			<< setw(7) << bensin / (meter_idag - meter_ar);
+	return 0;
+}
